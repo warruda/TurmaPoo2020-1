5a8e4b91442ac7b1b98f82a4934435a35bc9e0f0
warruda <warruda@gmail.com>
2020-03-10 00:34:51 -0300

    Adicionei o arquivo via upload

---
+++ br/com/turma20201/Hello.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package br.com.turma20201;
+
+/**
+ * @author wosle
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+
+	}
+
+}
